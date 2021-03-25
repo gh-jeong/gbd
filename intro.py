522dc1eac4fd106cc01cf6cee5a62dabf6f87c85
ghjeong <gwangh.jeong@gmail.com>
2021-03-25 20:36:48 +0900

    Codes for introduction of codes

---
+++ intro.py
@@ -0,0 +1,14 @@
+'''
+This is a sample design of the introduction of pygbd
+'''
+
+print(
+    '''
+    ------
+    Welcome to the pygbd!!
+    version: 1.0.0 (last updated: 21/03/25)
+    ------
+    '''
+)
+
+data = input("Insert your dataset name with address:\n")
