9d109128f1f49155185b72004e37de40b53b4312
ghjeong <gwangh.jeong@gmail.com>
2021-03-25 20:36:24 +0900

    test.py not more tracked

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
-columns = ["Hello", "World"]
+import pandas as pd
 
-new_list = ["what"] + columns
-print(new_list)
+# s = pd.Series(["a", "b", "c"])
+# print(s)
+
+# s.append(["d"], ignore_index=True)
+# print(s)
+
+s = [["hello", 1], ["world", 2]]
+df = pd.DataFrame(s, columns=['measure', 'value'])
+# print(df)
+
+s1 = [["wow", 3], ["good", 4]]
+df1 = pd.DataFrame(s1, columns=['measure', 'value'])
+print(df1)
+
+df1.append(df, ignore_index=)
