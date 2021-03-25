4e2d40ca6e60a75e9ab5ea05728687e224053eb6
ghjeong <gwangh.jeong@gmail.com>
2021-03-25 19:58:37 +0900

    Function table_1 done (not fully)

--- GBDPython.py
+++ GBDPython.py
@@ -1,3 +1,4 @@
+## Importing modules
 try:
     import numpy as np
     import pandas as pd
@@ -19,35 +20,6 @@ def combine_val(val, lower, upper, n=2):
     return str(round(val, n)) + " (" + str(lower, n)) + ", " + str(round(upper, n)) + ")"
 
 
-def measure_index(measure):
-    '''
-    :param measure: measure_id (type: list)
-    '''
-    for i in len(measure):
-        if measure[i] == 1:
-            measure[i] = 'Deaths'
-        elif measure[i] == 2:
-            measure[i] = 'DALYs'
-        elif measure[i] == 3:
-            measure[i] = 'YLDs'
-        elif measure[i] == 4:
-            measure[i] = 'YLLs'
-        elif measure[i] == 5:
-            measure[i] = 'Prevalence'
-        elif measure[i] == 6:
-            measure[i] = 'Incidence'
-        elif measure[i] == 25:
-            measure[i] = 'Maternal mortality ratio'
-        elif measure[i] == 26:
-            measure[i] = 'Life expectancy'
-        elif measure[i] == 28:
-            measure[i] = 'HALE (Healthy life expectancy)'
-        elif measure[i] == 29:
-            measure[i] = 'Summary exposure value'
-        else:
-            measure[i] = 'UNKNOWNED MEASURE VARIABLE!!!'
-    return measure
-
 def table_1(data, cause, measure, risk=None): #todo Table 1
     '''
     :param data: dataset (Data format: dataframe)
@@ -56,17 +28,13 @@ def table_1(data, cause, measure, risk=None): #todo Table 1
     :param measure: list of measures (Data format: List)
     :return: table (as dataframe)
     '''
-    
-    list_for_df = []
+    df = pd.DataFrame(index = measure, column = cause)    
 
     if not risk:
         for cause in cause:
             for measure in measure:
                 value_t = gbd_retrieve(data, measure, "Global", age=22, metric=1, year=2019)
-                list_for_df = list_for_df + [[measure, value_t]]
-                
-                pass
-            pass
+                df[cause, measure] = value_t # add value_t in the right site of the dataframe
         pass
     else:
         pass
